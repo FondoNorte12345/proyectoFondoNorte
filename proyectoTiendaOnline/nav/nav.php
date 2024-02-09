c0abbaafc0d1646ce9ec9ff8feb72f009b31fd99
Madtore <73850831+Madtore@users.noreply.github.com>
2024-02-09 11:34:34 +0100

    enlace home del nav lleva a index, enlace de mi cuenta a mi cuenta

--- proyectoTiendaOnline/nav/nav.php
+++ proyectoTiendaOnline/nav/nav.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarColor01">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="#">Home
+                    <a class="nav-link active" href="http://localhost/PROJECTOTIENDAONLINE/proyectoFondoNorte/proyectoTiendaOnline/index.php">Home
                         <span class="visually-hidden">(current)</span>
                     </a>
                 </li>
@@ -19,16 +19,6 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Separated link</a>
-                    </div>
                 </li>
             </ul>
             <form class="d-flex">
